1de96ebd7c536a74af5d03625a147cb62df573ac
BrigittePadilla <martha.padilla@croydon.com.co>
2024-08-21 17:34:27 -0500

    Continuación Event Binding

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -12,9 +12,14 @@ export class PersonasComponent {
   
   deshabilitar = false;
   mensaje = 'No se ha agregado ninguna persona';
+  titulo = '';
 
   agregarPersona(){
     this.mensaje = 'Persona Agregada';
+  }
+
+  modificarTitulo(event: Event){
+    this.titulo = (<HTMLInputElement>event.target).value;
   }
 
 }
