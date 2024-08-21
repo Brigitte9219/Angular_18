6299797b8b8ed9433a65fba2aed1d1fe9d2d0292
BrigittePadilla <martha.padilla@croydon.com.co>
2024-08-21 17:43:24 -0500

    Two way binding

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PersonaComponent } from "../persona/persona.component";
+import { FormsModule } from "@angular/forms";
+
 
 @Component({
   selector: 'app-personas',
   standalone: true,
-  imports: [PersonaComponent],
+  imports: [PersonaComponent, FormsModule],
   templateUrl: './personas.component.html',
   styleUrl: './personas.component.css'
 })
@@ -12,7 +14,7 @@ export class PersonasComponent {
   
   deshabilitar = false;
   mensaje = 'No se ha agregado ninguna persona';
-  titulo = '';
+  titulo = 'Ingeniero';
 
   agregarPersona(){
     this.mensaje = 'Persona Agregada';
