f9035c97601335a1e123c249dd3e2b32fc2bd057
BrigittePadilla <martha.padilla@croydon.com.co>
2024-08-21 19:32:39 -0500

    Directiva *ngIf

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { PersonaComponent } from "../persona/persona.component";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-personas',
   standalone: true,
-  imports: [PersonaComponent, FormsModule],
+  imports: [PersonaComponent, FormsModule, CommonModule],
   templateUrl: './personas.component.html',
   styleUrl: './personas.component.css'
 })
@@ -15,9 +16,11 @@ export class PersonasComponent {
   deshabilitar = false;
   mensaje = 'No se ha agregado ninguna persona';
   titulo = 'Ingeniero';
+  mostrar =false;
 
   agregarPersona(){
-    this.mensaje = 'Persona Agregada';
+    this.mostrar = true;
+    this.mensaje = 'Persona agregada';
   }
 
   modificarTitulo(event: Event){
